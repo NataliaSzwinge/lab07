02884a0565149b1c4067050b147e66c386eff221
Natalia Szwinge <szwingenatalia@gmail.com>
2023-06-30 17:34:30 +0200

    Add files via upload

---
+++ Programcw1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Globalization;
+
+namespace cw1 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime currentDateTime = DateTime.Now;
+            //DateTime.Now.ToString("HH:mm:ss");
+            //Console.WriteLine(currentDateTime);
+
+            //wiem jak pobrać czas z systemu ale nie wiem jak to przerobić żeby obliczyć upływ czasu w sek, dlatego liczba sek jest na sztywno
+
+
+            int czasWSekundach = 332;
+            string sformatowanyCzas = FormatowanieCzasu(czasWSekundach);
+            Console.WriteLine(sformatowanyCzas);
+        }
+
+        private static string FormatowanieCzasu(int sekundy)
+        {
+            int godziny = sekundy / 3600;
+            int minuty = (sekundy % 3600) / 60;
+            int pozostaleSekundy = (sekundy % 3600) % 60;
+            string sformatowanyCzas = godziny.ToString("D2") + ":" + minuty.ToString("D2") + ":" + pozostaleSekundy.ToString("D2");
+            return sformatowanyCzas;
+        }
+    }
+}
---
+++ Programcw2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //wzor |AB| = pierwiastek z (x2-x1)^2 + (y2-y1)^2
+            
+            Oblicz(2, 3, 4, 5);
+
+            double Oblicz(int x1, int x2, int y1, int y2)
+            {
+                int nawiasX = x2 - x1;
+                int nawiasY = y2 - y1;
+                double potegaX = Math.Pow(nawiasX, 2);
+                double potegaY = Math.Pow(nawiasY, 2);
+                double wynik = Math.Sqrt(potegaX + potegaY);
+                Console.WriteLine(wynik);
+                return wynik;
+                
+            }
+        }
+    }
+}
